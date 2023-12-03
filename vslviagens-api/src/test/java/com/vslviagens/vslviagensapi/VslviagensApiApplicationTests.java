fd8be7c72ee2b7e755d251507a7899a58614a6a9
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-12-03 11:18:11 -0300

    Enviando estrutura base do projeto

---
+++ vslviagens-api/src/test/java/com/vslviagens/vslviagensapi/VslviagensApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vslviagens.vslviagensapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VslviagensApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
